cee697a99cdf295b6036459d861d62cbb455c9a7
alamhanz <alamhanz@gmail.com>
2025-01-05 09:59:40 +0900

    test update

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import lereng
 
-map_maker = lereng.chrmap()
-# df = lereng.datasample("nation_sample_population.csv")
-# map_maker.insert(df, metric="2021", level="provinsi", path="temp_viz")
+map_maker = lereng.chrmap(level="provinsi")
+df = lereng.datasample("nation_sample_population.csv")
+map_maker.insert(df, metric="2021", path="temp_viz")
 
-df = lereng.datasample("jabar_sample_data_kemiskinan.csv")
-map_maker.insert(df, metric="2023", level="kabupaten_kota", path="temp_viz")
+# df = lereng.datasample("jabar_sample_data_kemiskinan.csv")
+# map_maker.insert(df, metric="2023", level="kabupaten_kota", path="temp_viz")
